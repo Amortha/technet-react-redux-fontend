8ca11f02181f58bbc776a19e53778547353a6a46
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-07-13 12:30:24 +0600

    commant

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -3,14 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { usePostCommentMutation } from '@/redux/api/apiSlice';
-
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
+import { useGetCommentQuery, usePostCommentMutation } from '@/redux/api/apiSlice';
 
 interface IProps {
   id: string;
@@ -19,21 +12,21 @@ interface IProps {
 export default function ProductReview({ id }: IProps) {
   const [inputValue, setInputValue] = useState<string>('');
 
+  const  {data} = useGetCommentQuery(id,{refetchOnMountOrArgChange:true})
+
   const [postComment, { isLoading, isError, isSuccess }] =
     usePostCommentMutation();
 
-console.log(isLoading),
-console.log(isSuccess),
-console.log(isError)
+  console.log(isLoading), console.log(isSuccess), console.log(isError);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-   
+
     const options = {
-      id:id,
-      data:{comment: inputValue},
-    }
-   
+      id: id,
+      data: { comment: inputValue },
+    };
+
     postComment(options);
     setInputValue('');
   };
@@ -58,7 +51,7 @@ console.log(isError)
         </Button>
       </form>
       <div className="mt-10">
-        {dummyComments.map((comment, index) => (
+        {data?.comments?.map((comment:string, index:number) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
