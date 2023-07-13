322562dfe8f7778639a661cf51d707d2d671d316
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-07-13 18:03:34 +0600

    query

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -3,7 +3,8 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { useGetCommentQuery, usePostCommentMutation } from '@/redux/api/apiSlice';
+import { useGetCommentQuery, usePostCommentMutation } from '@/redux/features/products/productApi';
+
 
 interface IProps {
   id: string;
@@ -12,7 +13,7 @@ interface IProps {
 export default function ProductReview({ id }: IProps) {
   const [inputValue, setInputValue] = useState<string>('');
 
-  const  {data} = useGetCommentQuery(id,{refetchOnMountOrArgChange:true})
+  const  {data} = useGetCommentQuery(id,{refetchOnMountOrArgChange:true, pollingInterval:1000})
 
   const [postComment, { isLoading, isError, isSuccess }] =
     usePostCommentMutation();
